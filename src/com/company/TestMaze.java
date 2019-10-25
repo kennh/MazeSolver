263c6aada336a45ecc669bae81030b913a93e214
Kennh <kennon_h@hotmail.com>
2019-10-24 20:26:18 -0700

    added ability to add array length.

--- src/com/company/TestMaze.java
+++ src/com/company/TestMaze.java
@@ -2,23 +2,40 @@ package com.company;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.sql.SQLOutput;
 import java.util.*;
 
 public class TestMaze {
     public static void main(String[] args) throws FileNotFoundException {
-        try {
-            Scanner input = new Scanner(new File("newmaze"));
-            List<Integer> list = new ArrayList<Integer>();
-            while (input.hasNextInt()) {
-                list.add(input.nextInt());
-                System.out.println(list);
-            }
-            input.close();
+        System.out.println("What size maze would you like: ");
+        Scanner input = new Scanner(System.in);
+        int number = input.nextInt();
+
+//        try {
+//            Scanner inputFile = new Scanner(new File("newmaze"));
+//            List<Integer> list = new ArrayList<>();
+//            while (inputFile.hasNextInt()) {
+//                list.add(inputFile.nextInt());
+//                System.out.println(list);
+//            }
+//            inputFile.close();
+//        }
+//        catch(FileNotFoundException e) {
+//            e.printStackTrace();
+//        }
+//    }
+        Scanner inputFile = new Scanner(new File("newmaze"));
+        ArrayList<Integer> list = new ArrayList<>();
+        ArrayList<ArrayList<Integer>> list2d = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i < number + 1; i++) {
+            list.add(inputFile.nextInt());
+            list2d.add(list);
+        //
         }
-        catch(FileNotFoundException e) {
-            e.printStackTrace();
-        }
+        System.out.println(list);
     }
 }
-
-// try and take input for length of the array ( maze size ).  make the array that length, the see if it can be solved as a puzzle.
+// take input for length of the array ( maze size ).
+// make the arraylist that length,
+// make the arraylist into  multidemensional array to create maze
+// see if maze can be solved.
